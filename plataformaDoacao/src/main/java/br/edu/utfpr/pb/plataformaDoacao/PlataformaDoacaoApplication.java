55e77fefa5a44f2578fba74b7d95df78dd18c9d3
Darley Krefta <darleykrefta@hotmail.com>
2018-09-12 21:47:50 -0300

    Adicionado o projeto padrão spring
    Foi adicionado o projeto padrão.

---
+++ plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/PlataformaDoacaoApplication.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.pb.plataformaDoacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDoacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDoacaoApplication.class, args);
+	}
+}
---
+++ plataformaDoacao/src/test/java/br/edu/utfpr/pb/plataformaDoacao/PlataformaDoacaoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.pb.plataformaDoacao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlataformaDoacaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

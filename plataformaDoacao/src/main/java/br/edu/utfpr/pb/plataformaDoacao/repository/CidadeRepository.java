78f718f3af3cf3892216fcbad5367845d7ea9b17
DaianaPilantir <42422999+DaianaPilantir@users.noreply.github.com>
2018-09-24 19:19:10 -0300

    Cidade
    Criado o CRUD de cidade

---
+++ plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/repository/CidadeRepository.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr.pb.plataformaDoacao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.utfpr.pb.plataformaDoacao.model.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long>{
+
+}

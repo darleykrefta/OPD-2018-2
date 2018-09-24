cf1ec64add6c56c751c5d8d65c694c5494e98a05
MatheusTonial <matheustonial@alunos.utfpr.edu.br>
2018-09-24 19:48:47 -0300

    inclusao de annotation

--- plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/controller/CategoriaController.java
+++ plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/controller/CategoriaController.java
@@ -1,5 +1,7 @@
 package br.edu.utfpr.pb.plataformaDoacao.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,7 @@ public class CategoriaController extends CrudController<Categoria, Long> {
 	@Autowired
 	private CategoriaService categoriaService;
 	
-	
+	@Valid
 	@Override
 	protected CrudService<Categoria, Long> getService() {
 		

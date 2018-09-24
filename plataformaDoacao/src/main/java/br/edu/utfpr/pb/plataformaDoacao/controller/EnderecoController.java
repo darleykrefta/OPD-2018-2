5a630720b180679276d84701c71ee65b678e1b0a
OsniDorini <osni@alunos.utfpr.edu.br>
2018-09-24 19:08:03 -0300

    Atualizacao 2.0
    corrigido mais erros, no controller

--- plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/controller/EnderecoController.java
+++ plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/controller/EnderecoController.java
@@ -30,38 +30,7 @@ public class EnderecoController extends CrudController<Endereco, Long> {
 	@Autowired
 	private EnderecoService enderecoService;
 	
-	@GetMapping(value="/")
-	public String listar(Model model) {
-		model.addAttribute("enderecos", enderecoRepository.findAll());
-		return "endereco/list";
-	}
 	
-	@GetMapping(value="/form")
-	public String novo(Model model) {
-		model.addAttribute("cidades", cidadeRepository.findAll());
-		model.addAttribute("endereco", new Endereco());
-		
-		return "endereco/form";
-	}
-	
-	@PostMapping(value="")
-	public String salvar(Endereco endereco) {
-		enderecoRepository.save(endereco);
-		return "redirect:/endereco/";
-	}
-	
-	@GetMapping(value="/{id}")
-	public String buscar(@PathVariable Long id, Model model) {
-		model.addAttribute("endereco", enderecoRepository.findById(id));
-		model.addAttribute("cidades", cidadeRepository.findAll());
-		return "endereco/form";
-	}
-	
-	@DeleteMapping(value = "/{id}")
-	public String remover(@PathVariable Long id) {
-		enderecoRepository.deleteById(id);
-		return "redirect:/endereco/";
-	}
 
 	@Valid
 	@Override

494e5ec54a6285e7f518e656089bf00c49be8efc
DaianaPilantir <42422999+DaianaPilantir@users.noreply.github.com>
2018-09-26 21:41:30 -0300

    Crud Cidade
    Alteração CRUD Cidade

--- plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/model/Cidade.java
+++ plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/model/Cidade.java
@@ -7,16 +7,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 
 @Entity
+@Table(name="cidade")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of= {"id"})
+@ToString
 public class Cidade implements Serializable{
 	
 	@Id

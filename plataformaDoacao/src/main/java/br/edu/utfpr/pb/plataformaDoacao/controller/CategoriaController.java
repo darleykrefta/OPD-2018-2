20cbbbbbd830c21a94415d5c8c900115479f9a7b
Vinicius Dalsasso <vin.dalsasso@gmail.com>
2018-10-29 21:32:44 -0300

    Corrigido projeto

--- plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/controller/CategoriaController.java
+++ plataformaDoacao/src/main/java/br/edu/utfpr/pb/plataformaDoacao/controller/CategoriaController.java
@@ -3,14 +3,14 @@ package br.edu.utfpr.pb.plataformaDoacao.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.utfpr.pb.plataformaDoacao.model.Categoria;
 import br.edu.utfpr.pb.plataformaDoacao.service.CategoriaService;
 import br.edu.utfpr.pb.plataformaDoacao.service.CrudService;
 
-@Controller
+@RestController
 @RequestMapping("/categoria")
 public class CategoriaController extends CrudController<Categoria, Long> {
 

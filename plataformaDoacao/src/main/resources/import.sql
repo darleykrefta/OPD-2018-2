3359dd2f8d49074d8e26e71fe3c48fb2f59bca81
MatheusTonial <matheustonial@alunos.utfpr.edu.br>
2018-11-27 21:53:07 -0200

    insert endereco

--- plataformaDoacao/src/main/resources/import.sql
+++ plataformaDoacao/src/main/resources/import.sql
@@ -5573,7 +5573,7 @@ Insert into cidade (id_cidade, nome, sigla) values (5222302,'Vila Propício', 'G
 Insert into cidade (id_cidade, nome, sigla) values (5300108,'Brasília', 'DF');
 INSERT INTO ENDERECO (rua, numero, bairro, cep, complemento, Id_Cidade) VALUES('rogerio da chaves', '1095', 'fraron', '85560408', 'casa' ,5222054);
 INSERT INTO ENDERECO (rua, numero, bairro, cep, complemento, Id_Cidade) VALUES('tupi', '1095', 'centro', '85560408', 'casa' ,5217203);
-INSERT INTO endereco(id_endereco, bairro, cep, complemento, numero, rua, id_cidade) VALUES (1, 'XYZ', '85560000', 'Ap 101', 272, 'ABC', 1100015);
+INSERT INTO endereco(bairro, cep, complemento, numero, rua, id_cidade) VALUES ('XYZ', '85560000', 'Ap 101', '272', 'ABC', 1100015);
 INSERT INTO categoria(id_categoria, nome) VALUES (1, 'Eletrônicos');
 INSERT into pessoa_institucao(apelido, cpf_cnpj, email, nome, senha, status, telefone, celular,id_endereco) values ('admin','999.999.999-99', 'admin', 'admin', '$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem',true, '99999-9999', '99999-9999',1);
 INSERT INTO pessoa_institucao_permissoes (pessoa_id_pessoa, permissoes_id) VALUES (1, 1);

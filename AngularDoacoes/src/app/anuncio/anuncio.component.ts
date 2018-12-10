9686a137aa5cbe2b9b8adbc12b59bb9d785ab9a1
Juan Hermann <35999705+JuanHermann@users.noreply.github.com>
2018-12-10 20:47:20 -0200

    att

--- AngularDoacoes/src/app/anuncio/anuncio.component.ts
+++ AngularDoacoes/src/app/anuncio/anuncio.component.ts
@@ -42,7 +42,7 @@ export class AnuncioComponent implements OnInit {
     private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.loginService.verificaAdmin();
+    this.loginService.verificaUsuarioLogado();
     this.categoriaService.findAll().subscribe(
       e => this.categorias = e);
     let id: string;
